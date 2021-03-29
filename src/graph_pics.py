4483c85e3eb14737266adee1ef5c632ea6344a6c
Tony GEORGE <tony.george@epita.fr>
2021-03-29 17:30:34 +0200

    [FEAT][GRAPH] Graphing all keys in data path.

--- src/graph_pics.py
+++ src/graph_pics.py
@@ -3,8 +3,8 @@ import matplotlib.pyplot as plt
 
 def graph_pics(name, info, pics):
     plt.plot(range(1, info["nb_pics"] + 1), pics[0], 'ko')
-    plt.xlabel('numéro de pic')
-    plt.ylabel('amplitude du pic')
+    plt.xlabel('Echantillon (toutes les 3ms)')
+    plt.ylabel('Tension')
     plt.title(name)
     plt.ylim(0, 1.5)
     plt.grid(b=True, which='both')
--- src/main.py
+++ src/main.py
@@ -1,23 +1,26 @@
 # Lib includes
 import matplotlib.pyplot as plt
+import os
 
 # File includes
 from read_pics import get_pics_from_file
 from graph_pics import graph_pics
 
+# Global Varibles
+DATA_PATH = "../data/"
+
 # Main
 if __name__ == "__main__":
     ######### Pics ############
 
-    # NO KEY
-    plt.figure(1)
-    pics_nokey, info_nokey = get_pics_from_file("../data/pics_NOKEY.bin")
-    graph_pics("pics_NOKEY", info_nokey, pics_nokey)
-
-    # PAD-0
-    plt.figure(2)
-    pics_pad0, info_pad0 = get_pics_from_file("../data/pics_0.bin")
-    graph_pics("pics_pad0", info_pad0, pics_pad0)
+    # Graphing all Data
+    ind_figure = 1
+    for filename in os.listdir(DATA_PATH):
+        print("\n=====\n{0}".format(filename))
+        plt.figure(ind_figure)
+        pics, info = get_pics_from_file("../data/" + filename)
+        graph_pics(filename, info, pics)
+        ind_figure += 1
 
     # Show graphs
     plt.show()
